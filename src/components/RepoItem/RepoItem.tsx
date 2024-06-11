d440d8bc48035ebebf22e785eab769564e0ded42
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2024-06-11 21:19:05 +0200

    some fixes

--- src/components/RepoItem/RepoItem.tsx
+++ src/components/RepoItem/RepoItem.tsx
@@ -8,24 +8,35 @@ interface RepoItemProps {
 
 export const RepoItem = ({ repo }: RepoItemProps) => {
   return (
-    <Box bg={"gray.300"} p={"2"} w={"90%"} textAlign={"left"}>
-      <Link href={repo.htmlUrl} isExternal _hover={{ textDecoration: "none" }}>
-        <Flex
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          fontWeight={"600"}
-          gap={"2"}
-        >
-          <Text fontSize={"xl"} wordBreak={"break-all"}>
-            {repo.name}
-          </Text>
-          <Flex alignItems={"center"} gap={"1"}>
-            <Text fontSize={"l"}>{repo.stargazersCount}</Text>
-            <FaStar />
-          </Flex>
+    <Link
+      href={repo.htmlUrl}
+      isExternal
+      _hover={{ textDecoration: "none" }}
+      bg={"gray.300"}
+      p={"2"}
+      w={"90%"}
+      textAlign={"left"}
+      height={"24"}
+    >
+      <Flex
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        fontWeight={"600"}
+        gap={"2"}
+      >
+        <Text fontSize={"xl"} wordBreak={"break-all"}>
+          {repo.name}
+        </Text>
+        <Flex alignItems={"center"} gap={"1"}>
+          <Text fontSize={"l"}>{repo.stargazersCount}</Text>
+          <FaStar />
         </Flex>
-        {repo.description && <Text>{repo.description}</Text>}
-      </Link>
-    </Box>
+      </Flex>
+      {repo.description && (
+        <Text h={"12"} overflow={"hidden"}>
+          {repo.description}
+        </Text>
+      )}
+    </Link>
   );
 };
